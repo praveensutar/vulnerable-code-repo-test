37cc91013f290e7e69ee7427b93e2cf5bc9f79d3
praveensutar <praveenstr60@gmail.com>
2025-02-12 11:10:22 +0530

    changes in code

--- codetest.py
+++ codetest.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request
 app = Flask(__name__)
 
-# curl -X GET "http://localhost:5000/myurl/touch%20HELLO"
+# curl -X GET "http://localhost:8000/myurl/touch%20HELLO"
 @app.route("/myurl/<something>")
 def test_sources_7(something):
     
